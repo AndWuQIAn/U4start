38718bc0bb88eb58bd09f2225cad44f11d11634a
Try <andwuqian@163.com>
2020-06-15 00:18:01 +0800

    day_0615 提交的msg

---
+++ day_0615/src/main/java/cn/kgc/day_0615/Day0615Application.java
@@ -0,0 +1,13 @@
+package cn.kgc.day_0615;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day0615Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day0615Application.class, args);
+    }
+
+}
